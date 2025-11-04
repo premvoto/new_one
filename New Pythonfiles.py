7bae9e6647e1add620cddf872a97d2ef63489953
premvoto <prem@votoconsulting.com>
2025-11-05 04:16:58 +0530

    Update New Pythonfiles.py

--- New Pythonfiles.py
+++ New Pythonfiles.py
@@ -1,42 +1,18 @@
-# # # user details
-# # name = input("Enter your name: ")
-# # age = int(input("Enter your age: "))
-# # location = input("Enter your location: ")
-# # address = input("Enter your address: ")
-# # salary = float(input("Enter your monthly salary: "))
+user details
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+address = input("Enter your address: ")
+salary = float(input("Enter your monthly salary: "))
 
-# # annual_salary = salary * 12
-
-
-# # print("\n--- User Details ---")
-# # print(f"Name: {name}")
-# # print(f"Age: {age}")
-# # print(f"Location: {location}")
-# # print(f"Address: {address}")
-# # print(f"Monthly Salary: ${salary:.2f}")
-# # print(f"Total Annual Salary: ${annual_salary:.2f}")
-
-# b = b"hello"          # bytes literal
-# b2 = bytes([104, 101, 108, 108, 141])  # from list of integers (0–255)
+annual_salary = salary * 12
 
 
-# print(b)
-# print(b2)
-
-# text = "hi"
-
-# print()
-# data = text.encode()     # turns letters into numbers
-# print(data)    
-# print(list(data))
-# print(len(data))
-
-# print("h".encode()) 
-# dataa = bytearray(b"hello")
-# print(dataa)
-# print(list(dataa))
-# print(len(dataa))
-# 
-
-print(type(5.0) == int)
+print("\n--- User Details ---")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Location: {location}")
+print(f"Address: {address}")
+print(f"Monthly Salary: ${salary:.2f}")
+print(f"Total Annual Salary: ${annual_salary:.2f}")
 
